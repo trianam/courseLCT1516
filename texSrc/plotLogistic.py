0875d4a3251c4e55bc2da849daf96e377f8d39b8
Stefano Martina <stefano.martina@gmail.com>
2015-09-28 01:50:24 +0200

    finished exam test.

---
+++ texSrc/plotLogistic.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+ax = plt.subplot(111)
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+
+k = 100.
+p0 = 10.
+l = 0.2
+
+t = np.arange(0.0, 50.0, 0.01)
+p = k / (1. + ((k-p0)/p0) * np.exp(-l*t))
+plt.plot(t, p, lw=2)
+
+plt.xlabel('$t$')
+plt.ylabel('$p(t)$')
+plt.title('$p(t)$ for $p_0 = 10$ and $\lambda = 0.2$')
+
+plt.ylim(0.,101.)
+plt.show()
